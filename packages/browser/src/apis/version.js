f0cec52f0e6dcc062095c5a7c551b636762ab47a
Siddharth Kothari <siddharthlatest@gmail.com>
2024-09-28 20:39:44 +0530

    fix: improves robustness for fetching search engine version
    - uses GET /, tries GET /${index}/_settings as a fallback

--- packages/browser/src/apis/version.js
+++ packages/browser/src/apis/version.js
@@ -8,62 +8,96 @@ import {
 
 export default async (rawUrl, indexName) => {
 	const defaultError = 'Unable to get version';
+	let version = '';
+
 	try {
 		const { url } = parseUrl(rawUrl);
 		const headers = getHeaders(rawUrl);
 
-		let fetchUrl = url;
-		let fetchHeaders = {};
-
-		if (indexName) {
-			fetchUrl = `${url}/${indexName}/_settings`;
-			fetchHeaders = convertArrayToHeaders(getCustomHeaders(indexName));
-		}
-
-		const res = await fetch(fetchUrl, {
+		// Main logic: Fetch `${url}/` and read `version.number`
+		const res = await fetch(url, {
 			headers: {
 				...headers,
-				...fetchHeaders,
 			},
 			method: 'GET',
 		}).then(response => response.json());
 
 		if (res.status >= 400 || (res.error && res.error.code >= 400)) {
-			throw new CustomError(
-				JSON.stringify(res.error, null, 2),
-				`HTTP STATUS: ${res.status >= 400 ||
-					(res.error && res.error.code
-						? res.error.code
-						: 400)} - ${defaultError}`,
-			);
+			// If error, use fallback logic
+			throw new Error('Error fetching version from root endpoint');
 		}
 
-		let version = '';
+		version = res.version && res.version.number;
 
-		if (indexName) {
-			const defaultIndex = Object.keys(res)[0];
-			if (defaultIndex) {
-				version =
-					res[defaultIndex].settings.index.version.upgraded ||
-					res[defaultIndex].settings.index.version.created;
+		if (!version) {
+			// If version is not obtained, use fallback logic
+			throw new Error('Version not found in root endpoint response');
+		}
+	} catch (error) {
+		// Fallback logic
+		try {
+			const { url } = parseUrl(rawUrl);
+			const headers = getHeaders(rawUrl);
+
+			let fetchUrl = url;
+			let fetchHeaders = {};
+
+			if (indexName) {
+				fetchUrl = `${url}/${indexName}/_settings`;
+				fetchHeaders = convertArrayToHeaders(
+					getCustomHeaders(indexName),
+				);
+			}
+
+			const res = await fetch(fetchUrl, {
+				headers: {
+					...headers,
+					...fetchHeaders,
+				},
+				method: 'GET',
+			}).then(response => response.json());
+
+			if (res.status >= 400 || (res.error && res.error.code >= 400)) {
+				throw new CustomError(
+					JSON.stringify(res.error, null, 2),
+					`HTTP STATUS: ${res.status >= 400 ||
+						(res.error && res.error.code
+							? res.error.code
+							: 400)} - ${defaultError}`,
+				);
+			}
+
+			if (indexName) {
+				const defaultIndex = Object.keys(res)[0];
+				if (defaultIndex) {
+					version =
+						res[defaultIndex].settings.index.version.upgraded ||
+						res[defaultIndex].settings.index.version.created;
+				} else {
+					version = '7';
+				}
 			} else {
+				version = res.version && res.version.number;
+			}
+
+			if (!version) {
 				version = '7';
 			}
-		} else {
-			version = res.version.number;
+		} catch (fallbackError) {
+			throw new CustomError(
+				fallbackError.description || defaultError,
+				fallbackError.message,
+				fallbackError.stack,
+			);
 		}
-
-		// Handle opensearch version
-		if (!version || Number(version[0]) < 5) {
-			version = '7';
-		}
-
-		return version.replace(/\./g, '');
-	} catch (error) {
-		throw new CustomError(
-			error.description || defaultError,
-			error.message,
-			error.stack,
-		);
 	}
+
+	// Process version to get major version number
+	let majorVersion = version.split('.')[0];
+
+	if (!majorVersion) {
+		majorVersion = '7';
+	}
+
+	return majorVersion;
 };
